82d0edcd6d341033e8e1d7d8fecd66eaa95f04c0
Sahil Agrawal <91172344+Sahil-o-0@users.noreply.github.com>
2024-10-22 12:43:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from logics.api_logic import count_status, get_hrbp_data, proj_status
+from api_logic import count_status, get_hrbp_data, proj_status
 
 app = FastAPI()
 
